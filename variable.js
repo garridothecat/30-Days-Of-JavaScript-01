127344cc744021f964cebc1a74c8d6aa8765a4a2
João Garrido <107148935+garridothecat@users.noreply.github.com>
2023-06-01 14:24:30 +0100

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'João'; // first name of a person
+let lastName = 'Garrido'; // last name of a person
+let country = 'Portugal'; // country
+let city = 'Porto'; // capital city
+let age = 24; // age in years
+let isMarried = false;
+
+// Declaring variables with number values
+
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Garrido', //name of a person
+	job = 'Student',
+	live = 'Portugal';
